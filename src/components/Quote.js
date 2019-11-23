cd3185b9db45c7b90ad14a4a6769e233b6e94693
Ksinia <xiatian@mail.ru>
2019-11-23 14:01:45 +0100

    Like, dislike buttons and conditional style

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -7,11 +7,36 @@ class Quote extends Component {
     quoteAuthor: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired
   };
+
+  state = {
+    liked: null
+  };
+
+  handleLike() {
+    this.setState({ liked: true, style: { color: "green" } }); //style should be an object
+  }
+  handleDislike() {
+    this.setState({
+      liked: false,
+      style: { color: "red", textDecoration: "line-through" }
+    });
+  }
+
   render() {
     return (
       <div key={this.props.id}>
-        <p className="quote-text">{this.props.quoteText}</p>
-        <p className="quote-author">By: {this.props.quoteAuthor}</p>
+        <div style={this.state.style}>
+          <p className="quote-text">{this.props.quoteText}</p>
+          <p className="quote-author">By: {this.props.quoteAuthor}</p>
+        </div>
+        <div className="buttons">
+          <button className="like" onClick={() => this.handleLike()}>
+            :)
+          </button>
+          <button className="dislike" onClick={() => this.handleDislike()}>
+            :(
+          </button>
+        </div>
       </div>
     );
   }
